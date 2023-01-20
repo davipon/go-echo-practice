b51bc27e3c14e5cdecd630bfdaf4c5e2cc2b40e6
davipon <davipondev@gmail.com>
2023-01-20 21:10:21 +0800

    feat: add middleware and basic post route

--- main.go
+++ main.go
@@ -4,14 +4,26 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
 	e := echo.New()
+
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
 	e.GET("/users/:id", getUser)
+	e.POST("/users", postUser)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -19,4 +31,14 @@ func getUser(c echo.Context) error {
 	id := c.Param("id")
 
 	return c.String(http.StatusOK, id)
+}
+
+func postUser(c echo.Context) error {
+	u := new(User)
+
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, u)
 }
